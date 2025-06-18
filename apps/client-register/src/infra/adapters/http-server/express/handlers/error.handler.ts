9bc3f3b9d2660c8ae2bae56e6d7a99a998cb5b86
Daniel Soares <daniel.soares@me.com>
2025-06-18 20:17:32 -0300

    fix: error handler

--- apps/client-register/src/infra/adapters/http-server/express/handlers/error.handler.ts
+++ apps/client-register/src/infra/adapters/http-server/express/handlers/error.handler.ts
@@ -1,22 +1,19 @@
-import { BadRequestException, httpStatusCode, NotFoundException } from 'core'
+import { BadRequestException, ConflictException, httpStatusCode, NotFoundException } from 'core'
 import type { Response } from 'express'
 
 export function errorHandler(error: unknown, res: Response): void {
-  if (error instanceof Error) {
-    switch (error.name) {
-      case BadRequestException.name:
-        res.status(httpStatusCode.BAD_REQUEST).json({ error: error.message })
-        break
-      case NotFoundException.name:
-        res.status(httpStatusCode.NOT_FOUND).json({ error: error.message })
-        break
-      default:
-        res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' })
-    }
-  } else {
-    console.error('Unexpected error:', error)
-    res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
-      error: 'Internal Server Error'
-    })
+  switch (true) {
+    case error instanceof BadRequestException:
+      res.status(httpStatusCode.BAD_REQUEST).json({ error: error.message })
+      break
+    case error instanceof NotFoundException:
+      res.status(httpStatusCode.NOT_FOUND).json({ error: error.message })
+      break
+    case error instanceof ConflictException:
+      res.status(httpStatusCode.CONFLICT).json({ error: error.message })
+      break
+    default:
+      console.error('Unexpected error:', error)
+      res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' })
   }
 }

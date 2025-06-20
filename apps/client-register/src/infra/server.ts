1f3d9da3bd199fa0e6007885c43e843468182169
Daniel Soares <daniel.soares@me.com>
2025-06-20 03:13:15 -0300

    chore(ci): add gh action

--- apps/client-register/src/infra/server.ts
+++ apps/client-register/src/infra/server.ts
@@ -4,6 +4,6 @@ import { serverModule } from './server.module'
 
 export async function server(): Promise<void> {
   const PORT = env.SERVER_PORT
-  const server = await serverModule()
+  const { server } = await serverModule()
   await server.listen(PORT, () => logger.info(`Server is running on port ${PORT}`))
 }
